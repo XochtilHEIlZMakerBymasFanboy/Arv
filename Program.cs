c3287773f7bc885347a193eccc5e2ba4f4273ed0
XochtilHEIlZMakerBymasFanboy <89978848+XochtilHEIlZMakerBymasFanboy@users.noreply.github.com>
2021-10-20 10:42:57 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Arv
+{
+    class Person
+    {
+        private string namn;
+        private string telefon;
+
+        public Person(string namn, string telefon)
+        {
+            this.namn = namn;
+            this.telefon = telefon;
+        }
+
+        public string GetNamn()
+        {
+            return namn;
+        }
+        public string GetTelefon()
+        {
+            return telefon;
+        }
+    }
+    class Student : Person
+    {
+        int inskrivningsår;
+        bool betaldKåravgift;
+
+        public Student(string n, string t, int år, bool betalat) : base(n, t)
+        {
+            inskrivningsår = år;
+            betaldKåravgift = betalat;
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine("Namn: " + base.GetNamn());
+            Console.WriteLine("TelNr: " + base.GetTelefon());
+            if (betaldKåravgift)
+            {
+                Console.WriteLine("Kåravgiften är betald!");
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                Student s = new Student("Lisa", "123454", 2005, true);
+                string n = s.GetNamn();
+                s.PrintInfo();
+            
+        }
+    }
+}
